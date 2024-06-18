d39645ef3ee64199f9cf990741379c1da940d696
alihaidar-us <91206967+alihaidar-us@users.noreply.github.com>
2024-06-18 17:59:19 -0400

    Added ec2 resource

--- hcp_tf/ec2.tf
+++ hcp_tf/ec2.tf
@@ -1,1 +1,13 @@
+resource "aws_instance" "my_1st_ec2" {
+    ami           = "ami-07caf09b362be10b8"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "my-1st-ec2"
+        Purpose = "Udemy"
+        Tier = "TF"
+    }
+}
 
+provider "aws" {
+    region     = "us-east-1"
+}
